5a761d9e68ec68f9dcbc43d10f59ddd1c845ca4b
Denis Toledo <contato@denistoledo.com.br>
2019-04-20 20:04:00 -0300

    first deploy

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,9 +6,7 @@ module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     prember: {
       urls: [
-        '/',
-        'portfolio',
-        'contact'
+        '/'
       ]
     },
     svgJar: {
