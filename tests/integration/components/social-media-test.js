827a8d78f01c2011327b1530249e8ec82b3d41fb
Denis Toledo <contato@denistoledo.com.br>
2019-07-20 23:11:53 -0300

    pr review

--- tests/integration/components/social-media-test.js
+++ tests/integration/components/social-media-test.js
@@ -11,10 +11,8 @@ module('Integration | Component | social-media', function (hooks) {
 
     assert.dom('[data-test-social-media="github"]').exists();
     assert.dom('[data-test-social-media="github"]').hasAttribute('href', 'https://github.com/dnstld');
-    assert.dom('[data-test-social-media="twitter"]').exists();
-    assert.dom('[data-test-social-media="twitter"]').hasAttribute('href', 'https://twitter.com/dnstld');
     assert.dom('[data-test-social-media="instagram"]').exists();
-    assert.dom('[data-test-social-media="instagram"]').hasAttribute('href', 'https://www.instagram.com/denistoledodev');
+    assert.dom('[data-test-social-media="instagram"]').hasAttribute('href', 'https://www.instagram.com/denistoledo.dev');
     assert.dom('[data-test-social-media="linkedin"]').exists();
     assert.dom('[data-test-social-media="linkedin"]').hasAttribute('href', 'https://www.linkedin.com/in/denistoledo');
   });
